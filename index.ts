fbe2730a3c7713fadd3b1349f70f80a415644667
Quentin Burg <quentin.burg@thetribe.io>
2023-01-09 23:58:20 +0100

    :tada:

---
+++ index.ts
@@ -0,0 +1,62 @@
+import { Player } from './types/player';
+import { Point, PointsData, Score } from './types/score';
+// import { none, Option, some, match as matchOpt } from 'fp-ts/Option';
+// import { pipe } from 'fp-ts/lib/function';
+
+// -------- Tooling functions --------- //
+
+export const playerToString = (player: Player) => {
+  switch (player) {
+    case 'PLAYER_ONE':
+      return 'Player 1';
+    case 'PLAYER_TWO':
+      return 'Player 2';
+  }
+};
+export const otherPlayer = (player: Player) => {
+  switch (player) {
+    case 'PLAYER_ONE':
+      return 'PLAYER_TWO';
+    case 'PLAYER_TWO':
+      return 'PLAYER_ONE';
+  }
+};
+// Exercice 1 :
+export const pointToString = (point: Point): string =>
+  'You can use pattern matching with switch case pattern.';
+
+export const scoreToString = (score: Score): string =>
+  'You can use pattern matching with switch case pattern.';
+
+export const scoreWhenDeuce = (winner: Player): Score => {
+  throw new Error('not implemented');
+};
+
+export const scoreWhenAdvantage = (
+  advantagedPlayed: Player,
+  winner: Player
+): Score => {
+  throw new Error('not implemented');
+};
+
+export const scoreWhenForty = (
+  currentForty: unknown,
+  winner: Player
+): Score => {
+  throw new Error('not implemented');
+};
+
+export const scoreWhenGame = (winner: Player): Score => {
+  throw new Error('not implemented');
+};
+
+// Exercice 2
+// Tip: You can use pipe function from fp-ts to improve readability.
+// See scoreWhenForty function above.
+export const scoreWhenPoint = (current: PointsData, winner: Player): Score => {
+  throw new Error('not implemented');
+};
+
+export const score = (currentScore: Score, winner: Player): Score => {
+  throw new Error('not implemented');
+};
